060d4b0af52147f717fbf353032ba4da395f9621
BenjaminEbomwonyi <benjamin.ebomwonyi@g.austincc.edu>
2024-09-10 00:26:20 +0000

    homework 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,27 @@
 ﻿//write include statements
+#include"data_types.h" //tells C++ where to find multiply_number function
+#include<iostream>
 
 
 //write namespace using statement for cout
+using std::cout; using std::cin;
 
 
 int main()
 {
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	int result = multiply_numbers(num);
 	
+	cout<<"result: "<<result<<"\n";
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+
+	cout<<"result: "<<result<<"\n";
 
 	return 0;
 }
